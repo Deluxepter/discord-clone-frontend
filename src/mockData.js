93ea9512282ec3265b7fa9eb3f7f371423090537
deluxepter <deluxepter@gmail.com>
2022-11-23 13:31:16 -0800

    remove redundancy, adjust data, and add new methods

--- src/mockData.js
+++ src/mockData.js
@@ -1,137 +1,222 @@
-const servers = {
-    "servers": [
-        {
-            "server_id": "18984156",
-            "name": "East Wood High School",
-            "icon": "https://cdn.pixabay.com/photo/2016/01/19/01/42/library-1147815_960_720.jpg",
-        },
-    ]
-}
+/**
+ * Makeshift backend for the app.
+ * Structured for easy conversion to SQL.
+ **/
 
-const categories = {
-    "categories": [
-        {
-            "category_id": "189519",
-            "name": "General",
-            "server_id": "18984156"
-        },
-        {
-            "category_id": "891569",
-            "name": "Voice",
-            "server_id": "18984156"
-        },
-    ]
-}
+const servers = [
+    {
+        "server_id": "18984156",
+        "name": "East Wood High School",
+        "icon": "https://cdn.pixabay.com/photo/2016/06/13/07/59/pi-1453836_960_720.jpg",
+    },
+    {
+        "server_id": "498197145",
+        "name": "Boys Hangout",
+        "icon": "https://cdn.pixabay.com/photo/2016/12/23/07/00/game-1926905_960_720.png",
+    },
+    {
+        "server_id": "1859",
+        "name": "Wild Dogs",
+        "icon": "https://cdn.pixabay.com/photo/2016/01/12/08/00/doge-meme-1134954_960_720.jpg",
+    },
+]
 
-const channels = {
-    "channels": [
-        {
-            "channel_id": "channel1",
-            "type": "text",
-            "name": "rules",
-            "category_id": "189519",
-        },
-        {
-            "channel_id": "channel2",
-            "type": "text",
-            "name": "announcements",
-            "category_id": "189519",
-        },
-        {
-            "channel_id": "channel3",
-            "type": "text",
-            "name": "support",
-            "category_id": "189519",
-        },
-        {
-            "channel_id": "channel4",
-            "type": "text",
-            "name": "feedback",
-            "category_id": "189519",
-        },
-        {
-            "channel_id": "channel5",
-            "type": "text",
-            "name": "discussion",
-            "category_id": "891569",
-        },
-        {
-            "channel_id": "channel6",
-            "type": "voice",
-            "name": "Homework-1",
-            "category_id": "891569",
-        },
-        {
-            "channel_id": "channel7",
-            "type": "voice",
-            "name": "Homework-2",
-            "category_id": "891569",
-        },
-        {
-            "channel_id": "channel8",
-            "type": "voice",
-            "name": "Lounge",
-            "category_id": "891569",
-        },
-    ]
-}
+const categories = [
+    {
+        "category_id": "1",
+        "name": "General",
+        "server_id": "18984156"
+    },
+    {
+        "category_id": "2",
+        "name": "Voice",
+        "server_id": "18984156"
+    },
+    {
+        "category_id": "3",
+        "name": "Hangout",
+        "server_id": "498197145"
+    },
+    {
+        "category_id": "4",
+        "name": "Welcome",
+        "server_id": "1859"
+    },
+]
 
-const user_servers = {
-    list: [
-        {
-            "id": "18984156",
-        },
-        {
-            "id": "25928491",
-        },
-    ]
-}
+const channels = [
+    {
+        "channel_id": "channel1",
+        "type": "text",
+        "name": "rules",
+        "category_id": "1",
+    },
+    {
+        "channel_id": "channel2",
+        "type": "text",
+        "name": "announcements",
+        "category_id": "1",
+    },
+    {
+        "channel_id": "channel3",
+        "type": "text",
+        "name": "support",
+        "category_id": "1",
+    },
+    {
+        "channel_id": "channel4",
+        "type": "text",
+        "name": "feedback",
+        "category_id": "1",
+    },
+    {
+        "channel_id": "channel5",
+        "type": "text",
+        "name": "discussion",
+        "category_id": "2",
+    },
+    {
+        "channel_id": "channel6",
+        "type": "voice",
+        "name": "Homework-1",
+        "category_id": "2",
+    },
+    {
+        "channel_id": "channel7",
+        "type": "voice",
+        "name": "Homework-2",
+        "category_id": "2",
+    },
+    {
+        "channel_id": "channel8",
+        "type": "voice",
+        "name": "Lounge",
+        "category_id": "2",
+    },
+    {
+        "channel_id": "channel9",
+        "type": "voice",
+        "name": "greeints",
+        "category_id": "3",
+    },
+    {
+        "channel_id": "channel10",
+        "type": "text",
+        "name": "rules",
+        "category_id": "4",
+    },
+]
 
-const server_users = {
-    users: [
-        {
-            "name": "bop",
-            "avatar_path": "https://cdn.discordapp.com/avatars/252622787724705803/65ef2bf9dd125d39fbd32c3dd649428a.webp?size=32"
-        },
-        {
-            "name": "Grawnpa",
-            "avatar_path": "https://cdn.discordapp.com/avatars/363093542182322188/7061bd6d1b2fb790d838fa9a64d0a0b4.webp?size=32"
-        },
-        {
-            "name": "skatina",
-            "avatar_path": "https://cdn.discordapp.com/avatars/417079462287376404/c0eb4e9c7894ce234022d2d67394da8b.webp?size=32"
-        },
-        {
-            "name": "spak5",
-            "avatar_path": "https://cdn.discordapp.com/avatars/307641799302840320/16e2b6009bdac0b5c7b469317d67635d.webp?size=32"
-        },
-    ]
-}
+const users = [
+    {
+        "id": "1",
+        "name": "bop",
+        "avatar_path": "https://cdn.pixabay.com/photo/2022/10/28/11/14/leaves-7552915_960_720.png",
+        "tag": "8954",
+        "status": "offline",
+        "description": "doing homework no talk"
+    },
+    {
+        "id": "2",
+        "name": "Grawnpa",
+        "avatar_path": "https://cdn.pixabay.com/photo/2022/11/01/12/45/cormorant-7562187__340.jpg",
+        "tag": "1561",
+        "status": "online",
+        "description": ""
+    },
+    {
+        "id": "3",
+        "name": "skatina",
+        "avatar_path": "https://cdn.pixabay.com/photo/2022/11/04/12/00/golden-hour-7569709__340.jpg",
+        "tag": "9518",
+        "status": "dnd",
+        "description": "grinding"
+    },
+    {
+        "id": "4",
+        "name": "spak",
+        "avatar_path": "https://cdn.pixabay.com/photo/2022/07/13/16/25/cat-7319589__340.jpg",
+        "tag": "4684",
+        "status": "idle",
+        "description": ""
+    },
+]
+
+const user_servers = [
+    {
+        "user_id": "1",
+        "server_id": "18984156",
+    },
+    {
+        "user_id": "1",
+        "server_id": "498197145",
+    },
+    {
+        "user_id": "1",
+        "server_id": "1859",
+    },
+    {
+        "user_id": "2",
+        "server_id": "1859",
+    },
+    {
+        "user_id": "2",
+        "server_id": "18984156",
+    },
+    {
+        "user_id": "3",
+        "server_id": "18984156",
+    },
+    {
+        "user_id": "4",
+        "server_id": "18984156",
+    },
+]
 
 function getServer(serverId){
-    return servers.servers.find(server => server.server_id === serverId)
+    return servers.find(server => server.server_id == serverId)
+}
+
+function getServers(userId){
+    const server_ids = user_servers.filter(server => server.user_id == userId)
+    return server_ids.map(server => getServer(server.server_id))
 }
 
 function getCategory(categoryId){
-    return categories.categories.find(category => category.category_id === categoryId)
+    return categories.find(category => category.category_id === categoryId)
 }
 
 function getCategories(serverId){
-    return categories.categories.filter(category =>
-        function (){
-            return category.server_id === serverId
-        }
-    )
+    return categories.filter(category => category.server_id == serverId)
 }
 
 function getChannels(categoryId){
-    return channels.channels.filter(channel => channel.category_id === categoryId)
+    return channels.filter(channel => channel.category_id === categoryId)
 }
 
+function getUser(userId){
+    return users.find(user => user.id == userId)
+}
+
+function getOnlineUsers(serverId){
+    const userIds = user_servers.filter(server => server.server_id == serverId)
+    const users =  userIds.map(user => getUser(user.user_id))
+    return users.filter(user => user.status != "offline")
+}
+
+function getOfflineUsers(serverId){
+    const userIds = user_servers.filter(server => server.server_id == serverId)
+    const users =  userIds.map(user => getUser(user.user_id))
+    return users.filter(user => user.status == "offline")
+}
 
 export {
     getServer,
+    getServers,
     getCategory,
     getCategories,
     getChannels,
+    getUser,
+    getOnlineUsers,
+    getOfflineUsers
 }
